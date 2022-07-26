c46ca398c701c0f726a08bbacb96cc26038ad96c
julie <perianmodely.julie@gmail.com>
2022-07-26 16:39:21 +0400

    seeder

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class Admin extends Seeder
@@ -20,7 +21,7 @@ class Admin extends Seeder
             'name' => 'Admin',
             'email' => 'admin@test.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' =>  Hash::make('password'),
             'remember_token' => Str::random(10),
             'is_admin' => 1
         ]);
