8dca520c05030fad87d1ed9eb6640be7d3714288
julie <perianmodely.julie@gmail.com>
2022-07-30 16:21:29 +0400

    add logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,7 +24,7 @@
 
 
         <div class="container-fluid">
-            <a class="navbar-brand" style="font-family:'Lateef', cursive; font-size:40px" href="#">YUM'S</a>
+            <a class="navbar-brand" href="#"> <img src="/images/logo.png" style="width:102px ;height:48px"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
